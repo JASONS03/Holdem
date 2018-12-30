6149a4cc54c26b0470e8702f798e8dd4161b66e8
junlun <jx755@nyu.edu>
2018-12-29 23:52:18 -0600

    first commit

---
+++ src/main/java/com/junlun/holdem/Holdem/HoldemApplication.java
@@ -0,0 +1,14 @@
+package com.junlun.holdem.Holdem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoldemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoldemApplication.class, args);
+	}
+
+}
+
